7460f211bcf2e7d3e9030ed7b2813bcb40a2ff08
syauqi fuadi <syauqi.8elements@gmail.com>
2024-09-23 13:55:48 +0800

    update tanggal pada note item

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {showFormattedDate} from "../utils";
 
 function NoteItem({ note, deleteNote, toggleArchive }) {
     return (
       <div className='note-item'>
         <h3>{note.title}</h3>
         <p>{note.body}</p>
+        <p className="note-date">{showFormattedDate(note.createdAt)}</p>
         <button onClick={() => deleteNote(note.id)}>Hapus</button>
         <button className='archive-button' onClick={() => toggleArchive(note.id)}>
           {note.archived ? 'Pindahkan' : 'Arsipkan'}
